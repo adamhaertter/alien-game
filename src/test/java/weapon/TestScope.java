06e2adbbe832c16f315905103bf576be1f1f85b7
Brennan Mulligan <bcmulligan7@gmail.com>
2020-10-06 18:28:05 -0400

    Minor work done for Attachment Classes

---
+++ src/test/java/weapon/TestScope.java
@@ -0,0 +1,18 @@
+package weapon;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests the mechanisms and effects of the Scope class on weapons
+ * 
+ * @author Brennan Mulligan
+ */
+public class TestScope {
+  @Test
+  public void TestPistolAndScope() {
+    Weapon pistol = new Pistol();
+    assertEquals();
+  }
+}
