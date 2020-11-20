89f989fb8ab2d40c4a489f34350e18e3238ca10c
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-11-19 20:46:50 -0500

    Setup start of HasWeapon State

--- src/main/java/state/HasWeaponState.java
+++ src/main/java/state/HasWeaponState.java
@@ -1,4 +1,94 @@
 package state;
 
-public class HasWeaponState {
+import environment.Cell;
+import exceptions.EnvironmentException;
+import lifeform.LifeForm;
+
+public class HasWeaponState extends ActionState {
+
+  /**
+   * Creates a HasWeapon State
+   * @param ai AI
+   */
+  public HasWeaponState(AIContext ai) {
+    super((ai));
+  }
+
+  /**
+   * Execute the HasWeaponState's action
+   * and then sets the state to
+   */
+  @Override
+  public void executeAction() {
+    int x;
+    int y;
+
+    LifeForm target = null;
+    int distance = lifeform.getWeapon().getMaxRange();
+    // find if there is a target or not
+    if (lifeform.currentDirection.equalsIgnoreCase("north")) {
+      for (int i = 1; i <= distance; i++) {
+
+        if (lifeform.getRow() - i >= 0) {
+          target = e.getCell(lifeform.getRow() - i, lifeform.getCol()).getLifeForm();
+          if (target != null) {
+            i = distance + 1;
+          }
+        } else {
+          i = distance + 1;
+        }
+      }
+    } else if (lifeform.currentDirection.equalsIgnoreCase("south")) {
+      for (int i = 1; i <= distance; i++) {
+
+        if (lifeform.getRow() + i < e.cells.length) {
+          target = e.getCell(lifeform.getRow() + i, lifeform.getCol()).getLifeForm();
+          if (target != null) {
+            i = distance + 1;
+          }
+        } else {
+          i = distance + 1;
+        }
+      }
+    } else if (lifeform.currentDirection.equalsIgnoreCase("east")) {
+      for (int i = 1; i <= distance; i++) {
+
+        if (lifeform.getCol() + i < e.cells[0].length) {
+          target = e.getCell(lifeform.getRow(), lifeform.getCol() + i).getLifeForm();
+          if (target != null) {
+            i = distance + 1;
+          }
+        } else {
+          i = distance + 1;
+        }
+      }
+    } else if (lifeform.currentDirection.equalsIgnoreCase("west")) {
+      for (int i = 1; i <= distance; i++) {
+
+        if (lifeform.getCol() - i >= 0) {
+          target = e.getCell(lifeform.getRow(), lifeform.getCol() - i).getLifeForm();
+          if (target != null) {
+            i = distance + 1;
+          }
+        } else {
+          i = distance + 1;
+        }
+      }
+    }
+
+    if (target != null) {
+      if (lifeform.getWeapon().getCurrentAmmo() <= 0) {
+        context.setCurrentState(context.getOutOfAmmoState());
+      } else {
+        try {
+          lifeform.attack(target, (int) e.getDistance(lifeform, target));
+        } catch (EnvironmentException environmentException) {
+          environmentException.printStackTrace();
+        }
+      }
+    } else {
+      // search
+    }
+
+  }
 }
